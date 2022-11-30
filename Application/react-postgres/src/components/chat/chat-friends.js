7ec96c8ec350f36a399bcd24a7d35643d182e98b
DearAsiaNichole <asianichole87@gmail.com>
2022-11-30 14:16:52 -0500

    Designed list of friends component

---
+++ Application/react-postgres/src/components/chat/chat-friends.js
@@ -0,0 +1,17 @@
+import user1 from "../../images/user1.png";
+
+const ChatFriends = (props) => {
+    return <aside>
+        <h1>FRIEND LIST</h1>
+        {props.friends?.map(friend => { //js map function for arrays to loop over items/entries: and maps it to the div element
+            return <div className="card bg-light mb-3">
+                <img src={user1} className="card-img-top with-small" alt="profile pic"/>
+                <div className="card-body">
+                    <h5 className="card-title">{friend.username}</h5>
+                </div>
+            </div>
+        })}
+    </aside>;
+};
+
+export default ChatFriends;
