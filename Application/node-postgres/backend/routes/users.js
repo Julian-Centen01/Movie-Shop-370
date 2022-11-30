581f32df8ddcaf9c44a22a568360823222ed8533
DearAsiaNichole <asianichole87@gmail.com>
2022-11-30 14:22:29 -0500

    Adding a route handler for getting a list of friends of a given user

--- Application/node-postgres/backend/routes/users.js
+++ Application/node-postgres/backend/routes/users.js
@@ -3,6 +3,16 @@ const movie_model = require("../models/movie_model");
 
 const router = Router();
 
+router.get('/:id/friends', (req, res) => {
+    movie_model.friendList(req.params)
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        });
+});
+
 router.get('/', (req, res) => {
     movie_model.getUser()
         .then(response => {
@@ -12,5 +22,6 @@ router.get('/', (req, res) => {
             res.status(500).send(error);
         });
 });
+
 
 module.exports = router;
