898984efb25cdf1bc49e41d7ae47b283f1e54aaa
DearAsiaNichole <asianichole87@gmail.com>
2022-12-08 14:50:26 -0500

    Creating user and friendships, to initialize the database

---
+++ Application/node-postgres/fixture.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, password) VALUES ('Asia',''),('Harrison', ''),('Madelyn',''),('Tom','');
+SELECT * FROM users;
+INSERT INTO friends (user_id, friend_id) VALUES (26,27),(26,28),(26,29),(27,26),(28,26),(28,29),(29,26),(29,28);
